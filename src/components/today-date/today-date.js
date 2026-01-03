10d17c26d884182fb6186e956e5ac317e790fe26
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-03 17:19:45 +0900

    chore: 불필요한 console.log 삭제

--- src/components/today-date/today-date.js
+++ src/components/today-date/today-date.js
@@ -1,11 +1,11 @@
-import { getDayofweek } from '@/utils/getDayofweek.js';
+import { getDayOfWeek } from '@/utils';
 
 export const TodayDate = () => {
   const $date = document.querySelector('.header__date');
   const today = new Date();
-  console.log(today);
   const year = today.getFullYear();
   const month = today.getMonth() + 1;
   const day = today.getDate();
-  $date.innerHTML = `${year}. ${month}. ${day}. ${getDayofweek(today)}`;
+
+  $date.innerHTML = `${year}. ${month}. ${day}. ${getDayOfWeek(today)}`;
 };
