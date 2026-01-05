738feecdfcb3d05b2eea71a7f6581cd5f8a47cc6
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-05 14:08:44 +0900

    feat: 특정 개수의 랜덤 숫자 생성 유틸 함수 추가

--- src/models/get-newspaper-for-rolling.js
+++ src/models/get-newspaper-for-rolling.js
@@ -1,15 +1,12 @@
 import { NUMBER_OF_NEWSPAPER } from '@/constants';
 import { fetchNewspaper } from '@/apis';
+import { getRandomNumberList } from '@/utils';
 
 export const getNewspaperForRolling = async () => {
-  const selectedNewspapers = new Set();
-
-  while (selectedNewspapers.size < 2) {
-    const randomNumber = new Uint8Array(1);
-    self.crypto.getRandomValues(randomNumber);
-    selectedNewspapers.add(randomNumber % NUMBER_OF_NEWSPAPER);
-  }
-  const [firstNewspaper, secondNewspaper] = Array.from(selectedNewspapers);
+  const [firstNewspaper, secondNewspaper] = getRandomNumberList({
+    max: NUMBER_OF_NEWSPAPER,
+    count: 2,
+  });
   const [firstNewspaperData, secondNewspaperData] = await Promise.all([
     fetchNewspaper(`_start=${firstNewspaper}&_limit=1`),
     fetchNewspaper(`_start=${secondNewspaper}&_limit=1`),
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,2 @@
 export { getDayOfWeek } from './get-day-of-week.js';
+export { getRandomNumberList } from './get-random-number-list.js';
