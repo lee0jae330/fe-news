cd41d2a8d9ee94476e1b072f2cd9af677c522f05
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-05 16:43:58 +0900

    feat: rolling section에 롤링 애니메이션 적용

--- src/components/rolling-section/rolling-section.js
+++ src/components/rolling-section/rolling-section.js
@@ -1,27 +1,96 @@
 import { getNewspaperForRolling } from '@/models';
-
+import { setIntervalForRolling } from '@/utils';
+import { rollingItemTemplate } from '@/constants';
+/**
+ * @typedef {import('../../types').Newspaper} Newspaper
+ */
 export const RollingSection = async () => {
-  const { firstNewspaperData, secondNewspaperData } =
-    await getNewspaperForRolling();
-  const firstNewspaperPress = firstNewspaperData.press;
-  const firstNewspaperArticles = firstNewspaperData.relatedArticles.slice(0, 5);
-  const secondNewspaperPress = secondNewspaperData.press;
-  const secondNewspaperArticles = secondNewspaperData.relatedArticles.slice(
-    0,
-    5,
-  );
+  /**
+   * @type {{ newspaperList: Newspaper[] }}
+   */
+  const { newspaperList } = await getNewspaperForRolling();
+  const leftSectionNewspaperList = newspaperList.slice(0, 5);
+  const rightSectionNewspaperList = newspaperList.slice(5, 10);
 
   const [$firstNewspaperItem, $secondNewspaperItem] = document.querySelectorAll(
     '.rolling-section__item',
   );
 
-  $firstNewspaperItem.innerHTML = `
-  <h1 class="rolling-section__item--newspaper">${firstNewspaperPress}</h1>
-  <p class="rolling-section__item--title">${firstNewspaperArticles[0].title}</p>
-  `;
+  let leftSectionNewspaperIndex = 0;
+  let rightSectionNewspaperIndex = 0;
 
-  $secondNewspaperItem.innerHTML = `
-  <h1 class="rolling-section__item--newspaper">${secondNewspaperPress}</h1>
-  <p class="rolling-section__item--title">${secondNewspaperArticles[0].title}</p>
-  `;
+  let leftCleanup = () => {};
+  let rightCleanup = () => {};
+  let timeoutId = null;
+
+  const startRolling = () => {
+    const { cleanup: firstCleanup } = setIntervalForRolling({
+      initialIndex: leftSectionNewspaperIndex,
+      newspaperList: leftSectionNewspaperList,
+      $newspaperItem: $firstNewspaperItem,
+      isRolling: true,
+    });
+    leftCleanup = firstCleanup;
+
+    timeoutId = setTimeout(() => {
+      const { cleanup: secondCleanup } = setIntervalForRolling({
+        initialIndex: rightSectionNewspaperIndex,
+        newspaperList: rightSectionNewspaperList,
+        $newspaperItem: $secondNewspaperItem,
+        isRolling: true,
+      });
+      rightCleanup = secondCleanup;
+    }, 1000);
+  };
+
+  const stopRolling = () => {
+    leftCleanup();
+    rightCleanup();
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      timeoutId = null;
+    }
+  };
+
+  $firstNewspaperItem.addEventListener('mouseenter', () => {
+    stopRolling();
+  });
+
+  $secondNewspaperItem.addEventListener('mouseenter', () => {
+    stopRolling();
+  });
+
+  $firstNewspaperItem.addEventListener('mouseleave', () => {
+    stopRolling();
+    startRolling();
+  });
+
+  $secondNewspaperItem.addEventListener('mouseleave', () => {
+    stopRolling();
+    startRolling();
+  });
+
+  $firstNewspaperItem.innerHTML = rollingItemTemplate({
+    shownNewspaperPress:
+      leftSectionNewspaperList[leftSectionNewspaperIndex].press,
+    shownNewspaperTitle:
+      leftSectionNewspaperList[leftSectionNewspaperIndex].mainTitle,
+    hiddenNewspaperPress:
+      leftSectionNewspaperList[leftSectionNewspaperIndex + 1].press,
+    hiddenNewspaperTitle:
+      leftSectionNewspaperList[leftSectionNewspaperIndex + 1].mainTitle,
+  });
+
+  $secondNewspaperItem.innerHTML = rollingItemTemplate({
+    shownNewspaperPress:
+      rightSectionNewspaperList[rightSectionNewspaperIndex].press,
+    shownNewspaperTitle:
+      rightSectionNewspaperList[rightSectionNewspaperIndex].mainTitle,
+    hiddenNewspaperPress:
+      rightSectionNewspaperList[rightSectionNewspaperIndex + 1].press,
+    hiddenNewspaperTitle:
+      rightSectionNewspaperList[rightSectionNewspaperIndex + 1].mainTitle,
+  });
+
+  startRolling();
 };
