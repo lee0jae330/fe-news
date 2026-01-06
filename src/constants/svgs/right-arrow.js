adbd0cfeb617fcc94e623b6ade62fd97716b8003
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-06 15:59:56 +0900

    feat: svg 관련 상수 폴더 추가 및 아이콘 svg 추가

---
+++ src/constants/svgs/right-arrow.js
@@ -0,0 +1,14 @@
+export const RIGHT_ARROW = `
+<svg
+                  width="24"
+                  height="40"
+                  viewBox="0 0 24 40"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0.320312 40.384L24.3203 20.384L0.320312 0.384033"
+                    stroke="currentColor"
+                  />
+                </svg>
+`;
