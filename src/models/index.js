cd76a438a2322878586fbeb08482c0515a9e4587
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-06 17:58:17 +0900

    feat: 화살표 버튼의 위치에 맞게 버튼 요소를 삽입하는 함수 추가

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,5 @@
 export { getNewspaperForRolling } from './get-newspaper-for-rolling';
 export { getNewspaperForGrid } from './get-newspaper-for-grid';
+export { createGridCardListHTML } from './create-grid-card-list-html';
+export { getArrowButtonPosition } from './get-arrow-buttons-position';
+export { insertArrowButtons } from './insert-arrow-buttons';
---
+++ src/models/insert-arrow-buttons.js
@@ -0,0 +1,43 @@
+import { leftArrowButtonTemplate, rightArrowButtonTemplate } from '@/templates';
+
+/**
+ * @typedef {Object} InsertArrowButtonsParams
+ * @property {HTMLElement} parentElement
+ * @property {'left' | 'right' | 'both'} position
+ * @property {string} leftButtonClassName
+ * @property {string} rightButtonClassName
+ *
+ * @param {InsertArrowButtonsParams} insertArrowButtonsParams
+ * @returns
+ */
+export const insertArrowButtons = ({
+  parentElement,
+  position,
+  leftButtonClassName,
+  rightButtonClassName,
+}) => {
+  if (position === 'left') {
+    parentElement.insertAdjacentHTML(
+      'beforeend',
+      leftArrowButtonTemplate({ className: leftButtonClassName }),
+    );
+  } else if (position === 'right') {
+    parentElement.insertAdjacentHTML(
+      'beforeend',
+      rightArrowButtonTemplate({ className: rightButtonClassName }),
+    );
+  } else {
+    insertArrowButtons({
+      parentElement,
+      position: 'left',
+      leftButtonClassName,
+      rightButtonClassName,
+    });
+    insertArrowButtons({
+      parentElement,
+      position: 'right',
+      leftButtonClassName,
+      rightButtonClassName,
+    });
+  }
+};
