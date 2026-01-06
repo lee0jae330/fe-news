92d088b363fd83965eed19624b7d149668478eb7
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-06 12:29:21 +0900

    chore: 랜덤값 생성 시 0~최댓값-1을 생성하도록 변경

--- src/utils/get-random-number-list.js
+++ src/utils/get-random-number-list.js
@@ -1,5 +1,5 @@
 /**
- * @description 0 ~ 최대 숫자 랜덤 숫자 생성
+ * @description 0 ~ 최대 숫자-1 랜덤 숫자 생성
  * @param {number} max
  * @param {number} count
  * @returns {number} 랜덤 숫자
@@ -9,7 +9,7 @@ export const getRandomNumberList = ({ max, count }) => {
   while (randomNumberSet.size < count) {
     const randomNumber = new Uint32Array(1);
     self.crypto.getRandomValues(randomNumber);
-    randomNumberSet.add(randomNumber % (max + 1));
+    randomNumberSet.add(randomNumber % max);
   }
   return Array.from(randomNumberSet);
 };
