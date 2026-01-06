f7e21694873e62a51fb87ccebc420640e7d838d0
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-06 11:51:18 +0900

    refactor: setInterval, setTimeout을 사용한 rolling 애니메이션 함수 제거

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,2 @@
 export { getDayOfWeek } from './get-day-of-week.js';
 export { getRandomNumberList } from './get-random-number-list.js';
-export { setIntervalForRolling } from './set-interval-for-rolling.js';
