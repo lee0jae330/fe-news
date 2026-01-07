4cc39ce5af3901257e8571831a365a5e781aee0e
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-07 17:35:08 +0900

    feat: import/export 정렬 관련 eslint 플러그인 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,10 @@
 
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
-import globals from 'globals';
 import prettier from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
+import sortImports from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 export default defineConfig(eslint.configs.recommended, prettier, {
   ignores: ['node_modules', 'dist'],
@@ -15,9 +16,24 @@ export default defineConfig(eslint.configs.recommended, prettier, {
   },
   plugins: {
     prettier: prettierPlugin,
+    'simple-import-sort': sortImports,
   },
   rules: {
     'prettier/prettier': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^\\u0000'],
+          ['^node:'],
+          ['^@?\\w'],
+          ['^@/'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
     'no-console': 'error',
     'no-unused-vars': [
       'error',
