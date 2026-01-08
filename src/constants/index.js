6ad7bfde9b3d50b44b0d1d6c5d28606da369667c
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-08 18:59:41 +0900

    feat: list view 전역 상태 상수 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,7 @@
 export { DAY_OF_WEEK } from './day-of-week.js';
 export { GRID_VIEW } from './grid-view.js';
+export { LIST_VIEW_INITIAL_STATE } from './list-view-state.js';
+export { NEWS_CATEGORY } from './news-category.js';
 export { NEWS_SECTION_STATE } from './news-section-state.js';
 export { NUMBER_OF_NEWSPAPER } from './number-of-newspaper.js';
 export { ROLLING_ANIMATION } from './rolling-animation.js';
