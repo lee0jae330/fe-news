66590c686f6cea3ad1597124a93d405a265b3bc4
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-08 15:44:46 +0900

    feat: 구독 해지 모달 이벤트 핸들러 추가
    - 모달 overlay 클릭 이벤트 핸들러
    - 모달 버튼 클릭 이벤트 핸들러
    - 모달 포커스 트랩, esc 적용 시 모달이 닫히도록 추가.

--- src/models/handlers/index.js
+++ src/models/handlers/index.js
@@ -1,3 +1,4 @@
 export { gridViewEventHandler } from './grid-view-event-handler';
 export { subscribedGridViewEventHandler } from './subscribed-grid-view-event-handler';
 export { totalGridViewEventHandler } from './total-grid-view-event-handler';
+export { unsubscriptionModalEventHandler } from './unsubscription-modal-event-handler';
